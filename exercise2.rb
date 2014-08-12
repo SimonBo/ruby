e68cfab9f929400abc4f0def74e0f54ef9e12db5
Szymon Borucki <szymon.borucki@gmail.com>
2014-08-12 12:48:01 -0400

    ex 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# task 1
+tip=55*0.15
+puts "You should give #{tip} to the sexy waitress"
+
+# task 2
+puts 'hello'+1.to_s
+
+# task 3
+result =45628 * 7839
+puts "The result of 45628 * 7839 is #{result}"
+
+#task 4
+puts (true && false) || (false && true) || !(false && false)
