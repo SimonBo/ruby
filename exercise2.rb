9320a58cbc5d96de070b21a0f7a07de113ef2d60
Szymon Borucki <szymon.borucki@gmail.com>
2014-08-12 13:24:00 -0400

    ex 4, added case to ex2

--- exercise2.rb
+++ exercise2.rb
@@ -1,13 +1,25 @@
 # task 1
 tip=55*0.15
-puts "You should give #{tip} to the sexy waitress"
-
+puts "Were you served by a sexy a waitress? (Yes/No?)"
+waitress = gets.chomp.downcase
+case waitress
+  when "yes"
+    puts "You should give #{tip} to the sexy waitress"
+  when "no"
+    puts "You should give #{tip} to the old hag"
+  else
+    puts "Plz answer Yes or No"
+end
 # task 2
+
+puts "TASK 2"
 puts 'hello'+1.to_s
 
 # task 3
+puts "TASK 3"
 result =45628 * 7839
 puts "The result of 45628 * 7839 is #{result}"
 
 #task 4
+puts "TASK 4"
 puts (true && false) || (false && true) || !(false && false)
--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,6 @@ puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
 
+puts "How old are you?"
+age=gets.chomp.to_i
+puts "#{age}?! Your are an old F**k!"
