4c638b0de875298415a6527d9512ec44fef77334
Szymon Borucki <szymon.borucki@gmail.com>
2014-08-13 13:28:08 -0400

    Finished ex 6

--- exercise6.rb
+++ exercise6.rb
@@ -6,6 +6,26 @@ def show_list(list)
     puts "* #{item}"
   end
   puts "Your list has #{list.count} items"
+
+  if list.include?("bananas")
+    puts "You need to pick up bananas"
+  else
+    puts "You don't need to pick up bananas today"
+  end
+
+  puts "The second item is #{list[1]}"
+
+  puts "Alphabetically sorted"
+  list.sort.each do |item|
+    puts "* #{item}"
+  end
+
+  puts "Alphabetically sorted (no salmon)"
+  list.delete("salmon")
+  list.sort.each do |item|
+    puts "* #{item}"
+  end
 end
+
 
 show_list(grocery_list)
